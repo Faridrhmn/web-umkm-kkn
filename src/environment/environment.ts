a5283bb59156abb60a9f306f271dc76fccd5ad88
rayhannaufal <rayhan.naufal55@gmail.com>
2025-01-24 13:43:16 +0700

    fix deploy issue

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,17 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyClS_PCknCBrf20DDm1xiIV1B3lbVIX0gU",
-        authDomain: "culture-mart.firebaseapp.com",
-        projectId: "culture-mart",
-        storageBucket: "culture-mart.appspot.com",
-        messagingSenderId: "543959998316",
-        appId: "1:543959998316:web:7517bb03cac556e4585d49"
+        // apiKey: "AIzaSyClS_PCknCBrf20DDm1xiIV1B3lbVIX0gU",
+        // authDomain: "web-umkm-tanjungrejo.firebaseapp.com",
+        // projectId: "web-umkm-tanjungrejo",
+        // storageBucket: "web-umkm-tanjungrejo.appspot.com",
+        // messagingSenderId: "543959998316",
+        // appId: "1:543959998316:web:7517bb03cac556e4585d49"
+        apiKey: "AIzaSyCXK1ikTPH8SlIJtqcI9M4fwbxjxTXFChc",
+        authDomain: "web-umkm-tanjungrejo.firebaseapp.com",
+        projectId: "web-umkm-tanjungrejo",
+        storageBucket: "web-umkm-tanjungrejo.firebasestorage.app",
+        messagingSenderId: "576890022136",
+        appId: "1:576890022136:web:9a476e7159bd35d542730d"
     }
 };
